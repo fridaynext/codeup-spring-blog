51f023caded3667062db361678ea39474c5cac2e
trantbatey <trantbatey@gmail.com>
2020-10-06 13:38:26 -0500

    Completed Views exercise.

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,21 +1,30 @@
-package com.codeup.blog;
+package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String showAllPosts() {
-        return "Here all the posts, my dude!";
+    public String showAllPosts(Model model) {
+        List<Post> postList = new ArrayList<>();
+        postList.add(new Post(0, "First Post", "This is the first Post"));
+        postList.add(new Post(0, "Second Post", "This is the second Post"));
+        model.addAttribute("posts", postList);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String showOnePost(@PathVariable long id) {
-        return String.format("Here is post number %d", id);
+    public String showOnePost(@PathVariable long id, Model model) {
+        Post post = new Post(0, "Single Post", "This is the boday for the single post");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
