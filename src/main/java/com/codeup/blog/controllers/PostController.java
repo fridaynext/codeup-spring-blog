85ecd43effe2f8209f2975a976ff9fee6780cc80
trantbatey <trantbatey@gmail.com>
2020-10-14 11:47:12 -0500

    Completed services exercise.

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -4,6 +4,7 @@ import com.codeup.blog.models.Post;
 import com.codeup.blog.models.User;
 import com.codeup.blog.repositories.PostRepository;
 import com.codeup.blog.repositories.UserRepository;
+import com.codeup.blog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,12 @@ import java.util.List;
 public class PostController {
     private final PostRepository postRepo;
     private final UserRepository userRepo;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postRepo, UserRepository userRepo) {
+    public PostController(PostRepository postRepo, UserRepository userRepo, EmailService emailService) {
         this.postRepo = postRepo;
         this.userRepo = userRepo;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -49,6 +52,22 @@ public class PostController {
 
     @PostMapping("/posts/create")
     public String createPost(@ModelAttribute Post post) {
+
+        // Send the create email
+        if (post.getId() == 0) {
+            post.setUser(userRepo.findAll().get(0)); // kluge to set a current user
+            emailService.prepareAndSend(post.getUser().getEmail(),
+                    "Created Post: " + post.getTitle(),
+                    post.getTitle() + "\n\n" + post.getBody());
+        }
+
+        // Send email for an edit
+        else {
+            post.setUser(postRepo.getPostById(post.getId()).getUser()); // Get the user from the database
+            emailService.prepareAndSend(post.getUser().getEmail(),
+                    "Edited Post: " + post.getTitle(),
+                    post.getTitle() + "\n\n" + post.getBody());
+        }
         postRepo.save(post);
         return "redirect:/posts/" + post.getId();
     }
@@ -56,6 +75,12 @@ public class PostController {
     @GetMapping("/posts/delete/{id}")
     public String deleteAd(@PathVariable long id) {
         Post post = postRepo.getPostById(id);
+        post.setUser(postRepo.getPostById(post.getId()).getUser()); // Get the user from the database
+
+        // send email for a post delete
+        emailService.prepareAndSend(post.getUser().getEmail(),
+                "Created Post: " + post.getTitle(),
+                post.getTitle() + "\n\n" + post.getBody());
         postRepo.delete(post);
         return "redirect:/posts";
     }
@@ -65,11 +90,5 @@ public class PostController {
         Post post = postRepo.getPostById(id);
         model.addAttribute("post", post);
         return "posts/create";
-    }
-
-    @PostMapping("/posts/edit")
-    public String updateAd(@ModelAttribute Post post) {
-        postRepo.save(post);
-        return "redirect:/posts/" + post.getId();
     }
 }
